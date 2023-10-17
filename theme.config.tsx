333882beaf62ae9eec5ff47659025660a04401d4
Garun Vagidov <garun@garunski.com>
2023-10-16 21:28:35 -0400

    cleanup setup instructions

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Scry Internal Documentation</span>,
   project: {
     link: "https://github.com/garunski-co/scry-commons",
   },
-  docsRepositoryBase: "https://github.com/garunski-co/scry-commons",
+  docsRepositoryBase: "https://github.com/garunski-co/scry-commons/tree/main",
   footer: {
     text: "Scry Internal Documentation",
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+      return {
+        titleTemplate: `${asPath !== "/" ? "%s – " : "" }Scry Internal Documentation"`,
+      };
   },
 };
 
